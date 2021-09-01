09c4303db047bd8082011bbea00106165297b473
rahul9318 <76800464+rahul9318@users.noreply.github.com>
2021-09-01 14:29:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from twilio.rest import Client
 import config
 
 app = Flask(__name__)
+account_sid = "AC6848ecabd02f695caeb51f5c118ef0c5"
+auth_token = "bf7057cb80d5532e70a717f64aa81ab9"
 
 # @app.route("/")
 # def home():
